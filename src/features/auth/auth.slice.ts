7c9042e8719dd0a8407ae18ae9019700b630bd5f
khovan <minhpnq1807@gmail.com>
2025-06-19 21:00:49 +0700

    <feat/common>: add common slice and type

--- src/features/auth/auth.slice.ts
+++ src/features/auth/auth.slice.ts
@@ -7,8 +7,8 @@ interface AuthState {
   role: 'INFLUENCER' | 'BRAND' | 'ADMIN' | null;
   token: string | null;
   id: string | null;
-  avatarUrl: string | null;
-  name?: string | null;
+  avatarUrl?: string | null;
+  name: string | null;
 }
 
 const initialState: AuthState = {
