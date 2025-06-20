7765f64b2577e6170c044f9e061edbe3a6814908
Nguyễn Ngọc Thiện <de180945nguyenngocthien@gmail.com>
2025-06-20 14:06:35 +0700

    Feat/forum api: response data to display follow by figma  (#35)
    * <feat/appilcation>: create navbar and search
    
    * <feat/myApplication>:design UI application follow by Figma
    
    * <feat/myApplication>: update pop-up for view detail
    
    * <feat/myApplication>: update pop-up for view detail
    
    * fix: fix pending application
    
    * <feat/myApplication>: fix UI
    
    * <chore>: commit to pull
    
    * <feat/application-api>: connect api 80%
    
    * <feat/application-api>: connect api 100%
    
    * <feat/forum-api>: fetch Api for forum but not fix createdDate yet
    
    * <fix/forum-api>: fetch api to application and api for forum
    
    * <fix/forum-api>: fetch api to application and api for forum
    
    * <fix/forum-api>: response data follow by figma
    
    ---------
    
    Co-authored-by: Kangnvd <ngocthiennguyen28052004@gmail.com>
    Co-authored-by: khang <khangnguyen.030204@gmail.com>

---
+++ src/features/application/accepted.tsx
@@ -0,0 +1,84 @@
+import { BadgeCheckIcon, Calendar } from 'lucide-react';
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Badge } from '@/components/ui/badge';
+import { Card, CardContent } from '@/components/ui/card';
+
+import ViewAccepted from './components/viewAccepted';
+
+
+const accepted = [
+  {
+     id: "1",
+    name: "Chiến dịch mỹ phẩm xuân 2025",
+    description: "Quảng bá dòng sản phẩm dưỡng da mới ra mắt cho mùa xuân.",
+    categories: ["Làm đẹp", "Chăm sóc da"],
+    avatar: "/images/campaigns/campaign1.jpg",
+    brand: "GlowUp Cosmetics",
+    createdDate: "01/03/2025",
+    appliedDate: "05/02/2025",
+    campaignRequirement: new Map([
+      ["followers", 10000],
+      ["posts", 3],
+      ["story", 5],
+    ]),
+    influencerRequirement: ["Nữ", "18-30 tuổi", "Quan tâm đến skincare"],
+    status: "accepted",
+  },
+  {
+    id: "2",
+    name: "Chiến dịch thời trang hè 2025",
+    description: "Tìm kiếm influencer để quảng bá bộ sưu tập thời trang hè năng động.",
+    categories: ["Thời trang", "Phong cách sống"],
+    avatar: "/images/campaigns/campaign2.jpg",
+    brand: "UrbanStyle",
+    createdDate: "20/05/2025",
+    appliedDate: "28/05/2025",
+    campaignRequirement: new Map([
+      ["followers", 5000],
+      ["reels", 2],
+      ["hashtag", 1],
+    ]),
+    influencerRequirement: ["Nam hoặc nữ", "Yêu thích phong cách đường phố", "Hoạt động trên Instagram"],
+    status: "accepted",
+  },
+];
+
+export default function Accepted() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-6 ">
+      {accepted.map((item) => (
+        <Card key={item.id} className="border shadow-sm flex items-center justify-center">
+          <CardContent className="w-full h-fit flex flex-col gap-4">
+            <div className="flex gap-4">
+              <Avatar className="h-12 w-12 mt-1">
+                <AvatarImage src={item.avatar || "/placeholder.svg"} alt={item.name} />
+                <AvatarFallback>{item.name.charAt(0)}</AvatarFallback>
+              </Avatar>
+              <div className="w-full">
+                <div className="flex items-center gap-2">
+                  <span className="font-semibold text-foreground flex-1">{item.name}</span>
+                  <Badge variant="accepted" className='w-30 rounded-full'>
+                    {' '}
+                    <BadgeCheckIcon />
+                    Đã chấp nhận
+                  </Badge>
+                </div>
+                <div className="text-muted-foreground text-sm mt-1">
+                  {item.brand} &bull; {item.createdDate}
+                </div>
+              </div>
+            </div>
+            <div className="flex items-center text-sm text-muted-foreground">
+              <Calendar className="w-4 h-4 mr-2 text-primary" />
+              <span>
+                Ngày ứng tuyển: <span className=" font-medium">{item.appliedDate}</span>
+              </span>
+            </div>
+              <ViewAccepted campaign={item}/>
+          </CardContent>
+        </Card>
+      ))}
+    </div>
+  );
+}
