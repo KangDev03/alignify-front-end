80ea68efe48d12dc688b78b3a5b4821c76fb3b39
Nguyen Van Duy Khang <134641429+KangDev03@users.noreply.github.com>
2025-06-23 16:04:07 +0700

    feat: campaign-management (#46)
    * feat: first commit
    
    * feat: fix UI & integrate API for campaign-management page
    
    * feat: change campaign-card button

---
+++ src/pages/campaign-management/index.tsx
@@ -0,0 +1,73 @@
+"use client"
+
+import { useState } from "react"
+
+import { Input } from "@/components/ui/input"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+
+import { Icons } from "@/components/icons/icons"
+import type { Campaign } from "@/features/common/common.type"
+import { useGetAllCampaignsOfBrandQuery } from "@/features/my-campaign/campaign.service"
+import CampaignCard from "@/features/my-campaign/components/campaign-card"
+
+const tabs = [
+  { value: "DRAFT", label: "Nháp" },
+  { value: "RECRUITING", label: "Đang tuyển" },
+  { value: "PENDING", label: "Chưa bắt đầu" },
+  { value: "PARTICIPATING", label: "Đang diễn ra" },
+  { value: "COMPLETED", label: "Đã kết thúc" },
+]
+
+export function CampaignManagement() {
+  const [activeTab, setActiveTab] = useState("DRAFT")
+
+  const { data: campaignsResponse } = useGetAllCampaignsOfBrandQuery({
+    pageNumber: 0,
+    pageSize: 10,
+  });
+  const campaigns: Campaign[] = Array.isArray(campaignsResponse?.data?.campaigns)
+    ? campaignsResponse.data.campaigns
+    : [];
+
+  console.log("campaignsResponse:", campaigns);
+
+
+  const filteredCampaigns = campaigns.filter((campaign) => campaign.status === activeTab);
+
+  return (
+    <div className="space-y-6">
+      <h1 className="text-3xl font-bold">Quản lí chiến dịch</h1>
+      <Tabs defaultValue="DRAFT" value={activeTab} onValueChange={setActiveTab} className="w-full gap-6">
+        <div className="flex flex-row gap-6">
+          <TabsList className="grid w-full h-fit grid-cols-5 p-1">
+            {tabs.map((tab) => (
+              <TabsTrigger
+                key={tab.value}
+                value={tab.value}
+                className="h-full"
+              >
+                {tab.label}
+              </TabsTrigger>
+            ))}
+          </TabsList>
+          <div className="relative w-2/5">
+            <Icons.search className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground" />
+            <Input placeholder="Tìm kiếm..." className="pl-8" />
+          </div>
+        </div>
+
+        <TabsContent value={activeTab}>
+          <div className="grid grid-cols-2 gap-4">
+            {filteredCampaigns.length > 0 ? (
+              filteredCampaigns.map((campaign) => (
+                <CampaignCard key={campaign.campaignId} campaign={campaign} />
+              ))
+            ) : (
+              <p className="text-muted-foreground text-sm">Không có chiến dịch nào.</p>
+            )}
+          </div>
+        </TabsContent>
+      </Tabs>
+    </div>
+  )
+}
