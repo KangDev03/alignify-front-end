14038ef3cbdb2dd7df5773336a4480c94b45956d
khang <khangnguyen.030204@gmail.com>
2025-07-04 16:30:58 +0700

    fix: fix to deploy

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,6 +1,5 @@
 import * as React from "react"
 import { cva, type VariantProps } from "class-variance-authority"
-import { motion } from "motion/react"
 
 import { cn } from "@/lib/utils"
 import { Slot } from "@radix-ui/react-slot"
