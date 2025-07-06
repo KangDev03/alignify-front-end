b581bf5a4e11f116fb3dcd7cd551c45a7839ec9c
Nguyễn Ngọc Thiện <de180945nguyenngocthien@gmail.com>
2025-07-07 00:29:20 +0700

    Fix/profile: fix display contact in brand profile (#77)
    * <feat/getFollower>: get follower from social media link -- sp: yt,fb,ins,tiktok
    
    * <feat/filterCategory>: filter for forum
    
    * <feat/filterCategory>: filter for brand
    
    * <feat/filterCategory>: filter for brand,influencer
    
    * <fix/format>: format follower number
    
    * <fix/profile>: fix display contact in brand profile
    
    ---------
    
    Co-authored-by: khovan <minhpnq1807@gmail.com>
    Co-authored-by: Kangnvd <ngocthiennguyen28052004@gmail.com>
    Co-authored-by: nhibao08 <lebaonhi0805@gmail.com>

--- src/pages/profile/brand-profile.tsx
+++ src/pages/profile/brand-profile.tsx
@@ -35,8 +35,8 @@ export function BrandProfilePage() {
 
   const profile = profileRaw?.data;
   const contactMap = Array.isArray(profile.contacts)
-    ? Object.fromEntries(profile.contacts.map((c: any) => [c.key, c.value]))
-    : profile.contacts || {};
+    ? Object.fromEntries(profile.contacts.map((c: any) => [c.contact_type, c.contact_infor]))
+    : {};
 
   // const getVerificationBadge = () => {
   //   switch (brand.verificationStatus) {
