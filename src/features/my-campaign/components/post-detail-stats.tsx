79ec8993416af35010b28a1da32e309cfefcf167
Phan Nguyễn Quốc Minh <152273723+khovan123@users.noreply.github.com>
2025-07-25 13:39:58 +0700

    <fix/postDetail>: fix null pointer (#110)
    * <feat/sound>: add sound when recieved notify
    
    * <feat/setting>: completed supported endpoint in setting
    
    * <fix/dialog>: prevent break layout and hide sensitive button in influencer
    
    * <fix/postDetail>: fix null pointer
    
    * <fix/postDetail>: fix null pointer

--- src/features/my-campaign/components/post-detail-stats.tsx
+++ src/features/my-campaign/components/post-detail-stats.tsx
@@ -89,11 +89,13 @@ export default function PostDetailStats({ form, req, reqIndex, contentIndex }: P
               <div className="flex gap-2 items-center">
                 <Input
                   readOnly={
+                    req.details[contentIndex] &&
                     req.details[contentIndex].status !== undefined &&
                     req.details[contentIndex].status !== null &&
                     req.details[contentIndex].status !== 'REJECTED'
                   }
                   disabled={
+                    req.details[contentIndex] &&
                     req.details[contentIndex].status !== undefined &&
                     req.details[contentIndex].status !== null &&
                     req.details[contentIndex].status !== 'REJECTED'
@@ -106,7 +108,8 @@ export default function PostDetailStats({ form, req, reqIndex, contentIndex }: P
                     field.onChange(e.target.value);
                   }}
                 />
-                {(!req.details[contentIndex].status ||
+                {(!req.details[contentIndex] ||
+                  !req.details[contentIndex].status ||
                   req.details[contentIndex].status === 'REJECTED') &&
                   (!hasError && !field.value ? null : isChecked ? (
                     <Button
